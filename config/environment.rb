f7c3b35002fc21cec0eb93450a89b8209e72fc94
Haseeb Khan <mhaseebkhan@gmail.com>
2017-01-25 19:32:50 +0500

    Add Routes

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
+require_relative '../lib/parser'
+
 
 # Initialize the Rails application.
 Rails.application.initialize!
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'urls/index'
 
-  get 'urls/create'
+  scope module: 'api' do
+    namespace :v1 do
+      resources :urls, only: [:index, :create]
+    end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
